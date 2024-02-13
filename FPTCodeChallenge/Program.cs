38a1971994938f429fb8ef3e6861fa57adda885d
patfinder <vuong_se@yahoo.com>
2024-02-13 11:12:42 -0500

    Add log and polly (retry).

--- FPTCodeChallenge/Program.cs
+++ FPTCodeChallenge/Program.cs
@@ -4,39 +4,51 @@ using IAdviseService;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualBasic;
+using Polly;
+using Polly.Extensions;
+using Polly.Extensions.Http;
+using Polly.Registry;
+using Polly.Retry;
 using WeatherService;
 using WeatherServiceInterface;
-
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        // Setup DI
-        //var serviceProvider = new ServiceCollection()
-        //    .AddSingleton<IWeatherForecast, WeatherForecast>()
-        //    .BuildServiceProvider();
-
-
         HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        // Add console log provider
+        builder.Logging.AddConsole();
 
         builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
         builder.Configuration.AddJsonFile("appsettings.json", optional: false);
         builder.Configuration.AddEnvironmentVariables(prefix: "PREFIX_");
         builder.Configuration.AddCommandLine(args);
 
-        builder.Services.AddSingleton<IWeatherForecast, WeatherForecast>();
+        builder.Services.AddSingleton<IWeatherForecast, WeatherForecast>(
+            (IServiceProvider provider) => new WeatherForecast(provider, builder.Configuration)
+        );
         builder.Services.AddSingleton<IAdviceService, AdviceService>();
+
+        // Register resilence pipeline
+        RegisterResilencePipeline(builder.Services);
 
         using IHost host = builder.Build();
 
-        await ShowQuery(host.Services);
+        await ShowAdvice(host.Services);
 
         await host.RunAsync();
     }
 
-    public static async Task ShowQuery(IServiceProvider serviceProvider)
+    public static async Task ShowAdvice(IServiceProvider serviceProvider)
     {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        // For log demonstration only
+        logger.LogWarning("Start program (log demo) .......");
+
         Console.Write("Please enter your zip code: ");
         var zipcode = Console.ReadLine();
         //var zipcode = "L5G";
@@ -53,5 +65,26 @@ public class Program
         Console.WriteLine($"Should I wear sunscreen? {wearSunscreen} (UvIndex: {advice.UvIndex})");
         Console.WriteLine($"Can I fly my kite? {flyKite} (WindSpeed: {advice.WindSpeed})");
         Console.WriteLine("========================================\n\n\n");
+    }
+
+    static void RegisterResilencePipeline(IServiceCollection services)
+    {
+        // Retry with back-off
+        // Define a resilience pipeline with the name "weather-pipeline"
+        services.AddResiliencePipeline("weather-pipeline", builder =>
+        {
+            var retryOptions = new RetryStrategyOptions
+            {
+                ShouldHandle = new PredicateBuilder().Handle<HttpRequestException>(),
+                BackoffType = DelayBackoffType.Exponential,
+                UseJitter = true,  // Adds a random factor to the delay
+                MaxRetryAttempts = 4,
+                Delay = TimeSpan.FromSeconds(2),
+            };
+
+            builder
+                .AddRetry(retryOptions)
+                .AddTimeout(TimeSpan.FromSeconds(10));
+        });
     }
 }
--- WeatherService/WeatherForecast.cs
+++ WeatherService/WeatherForecast.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Registry;
+using Polly.Timeout;
+using System;
 using System.Net;
 using System.Net.Http.Json;
 using WeatherDTO;
@@ -8,24 +14,67 @@ namespace WeatherService
 {
     public class WeatherForecast : IWeatherForecast
     {
+        private readonly ILogger<WeatherForecast> Logger;
+        private readonly IServiceProvider Services;
         private readonly IConfiguration Configuration;
 
-        public WeatherForecast(IConfiguration configuration)
+        public WeatherForecast(IServiceProvider services, IConfiguration configuration)
         {
+            Services = services;
             Configuration = configuration;
+            Logger = services.GetRequiredService<ILogger<WeatherForecast>>();
         }
 
         public async Task<WeatherForecastDTO> GetCurrentWeather(string location)
         {
-            var httpClient = new HttpClient();
+            return await GetWithRetry(location);
+        }
 
-            // http://api.weatherstack.com/
-            var baseUrl = Configuration.GetSection("WeatherAPI")["BaseURL"];
-            var accessKey = Configuration["WEATHER_API_KEY"];
-            var uri = $"{baseUrl}current?access_key={accessKey}&query={location}";
-            var weatherForecast = await httpClient.GetFromJsonAsync<WeatherForecastDTO>(uri);
+        private async Task<WeatherForecastDTO> GetWithRetry(string location)
+        {
+            // Resolve the resilience pipeline provider for string-based keys
+            ResiliencePipelineProvider<string> pipelineProvider = Services.GetRequiredService<ResiliencePipelineProvider<string>>();
+            ResiliencePipeline pipeline = pipelineProvider.GetPipeline("weather-pipeline");
 
-            return weatherForecast;
+            // Acquire a ResilienceContext from the pool
+            ResilienceContext context = ResilienceContextPool.Shared.Get();
+
+            // Should call LogInformation or LogDebug
+            // Use LogWarning here just for ===== DEMONSTRATION PURPOSE =====
+            Logger.LogWarning($"\n\n=============================================");
+            Logger.LogWarning($"Getting weather info for location: {location}\n\n");
+
+            // Execute the pipeline
+            // Notice in console output that telemetry is automatically enabled
+            var outcome = await pipeline.ExecuteOutcomeAsync<WeatherForecastDTO, string>(async (context, state) =>
+            {
+                var httpClient = new HttpClient();
+
+                // http://api.weatherstack.com/
+                var baseUrl = Configuration.GetSection("WeatherAPI")["BaseURL"];
+                var accessKey = Configuration["WEATHER_API_KEY"];
+                var uri = $"{baseUrl}current?access_key={accessKey}&query={state}";
+
+                var weatherForecast = await httpClient.GetFromJsonAsync<WeatherForecastDTO>(uri, context.CancellationToken);
+                return Outcome.FromResult(weatherForecast);
+            }, context, location);
+
+            // Return the acquired ResilienceContext to the pool
+            ResilienceContextPool.Shared.Return(context);
+
+            if (outcome.Exception != null)
+            {
+                Logger.LogError($"Weather API exception : {outcome.Exception.Message}");
+
+                throw outcome.Exception;
+            }
+            else
+            {
+                if (outcome.Result == null)
+                    throw new Exception("Unexpected error: weather result is empty.");
+
+                return outcome.Result;
+            }
         }
     }
 }
